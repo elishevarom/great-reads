c6b3737bb06559c57bb7cc6460863a346d1b7506
Menucha Rooshanshad <menucharooshanshad@gmail.com>
2024-07-26 10:48:03 -0400

    personal touch mr

--- greatreads/src/layout/layout.jsx
+++ greatreads/src/layout/layout.jsx
@@ -16,18 +16,10 @@ export function Layout() {
             <Navbar.Collapse id="navbar-nav" className="justify-content-center">
               <Nav>
                 <Nav.Link as={Link} to="/" className="text-light">Home</Nav.Link>
-                <Nav.Link as={Link} to="/about" className="text-light">About</Nav.Link>
-                <Nav.Link as={Link} to="/add" className="text-light">Add</Nav.Link>
-                <Nav.Link as={Link} to="/update" className="text-light">Update</Nav.Link>
-                <Nav.Link as={Link} to="/retrieve" className="text-light">Retrieve</Nav.Link>
-                <Nav.Link as={Link} to="/delete" className="text-light">Delete</Nav.Link>
-                <NavDropdown title="More" id="basic-nav-dropdown" className="text-light">
-                  <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link as={Link} to="/about" className="text-light">About Us</Nav.Link>
+                <Nav.Link as={Link} to="/addBook" className="text-light">Add a Book</Nav.Link>
+                <Nav.Link as={Link} to="/addReview" className="text-light">Add Your Review</Nav.Link>
+                <Nav.Link as={Link} to="/findBook" className="text-light">Find a Book</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
--- greatreads/src/pages/findBook.jsx
+++ greatreads/src/pages/findBook.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Dropdown, ButtonGroup } from 'react-bootstrap';
+import {Helmet} from 'react-helmet';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 
 export function Retrieve() {
     const [books, setBooks] = useState([]);
@@ -49,6 +52,14 @@ export function Retrieve() {
     
     return (
         <Container>
+            <div>
+                <Helmet>
+                    <title>Great Reads - Find a Book Us</title>
+                    <meta name="keywords" content="children's books, storybook, storytime, book reviews, good stories, kids book, free access, stories, story, reading level, elementary, preschool, middle school" />
+                    <meta property="og:title" content="Great Reads-Find a Book"/>
+                    <meta property="og:description" content="Find a book you know you'll love at the appropriate reading level for you." />
+                </Helmet>
+            </div>
             <div className="text-center mb-5">
                 <h2 className="display-4 font-weight-bold">Discover Our Books</h2>
                 <p className="lead">Select one of our books to view detailed information and read the latest reviews</p>
@@ -89,6 +100,60 @@ export function Retrieve() {
                     </ul>
                 </div>
             )}
+            <Carousel 
+                showThumbs={false} 
+                showStatus={false} 
+                infiniteLoop 
+                useKeyboardArrows 
+                autoPlay 
+                interval={3000}
+                swipeable
+                emulateTouch
+                top={0}
+                left={0}
+                width= {'100%'}
+                height = {'100%'}
+                >
+                <div>
+                    <img src="booktower.jpg" alt="A tower made form books and novels and stories"/>
+                </div>
+                <div>
+                    <img src="bookbutterflies.jpg" alt="Book or novel with charachters coming to life"/>
+                </div>
+                <div>
+                    <img src="heartbooks.jpg" alt="Story with a heart formed from pages"/>
+                </div>
+                <div>
+                    <img src="glowingbooks.jpg" alt="A glowing book"/>
+                </div>
+                <div>
+                    <img src="colorfulbooks.jpg" alt="A stack of colorful exciting books"/>
+                </div>
+                <div>
+                    <img src="astheticbook.jpg" alt="A calming asthetic book"/>
+                </div>
+                <div>
+                    <img src="blackandwhitebooks.jpg" alt="A stack of simple stories"/>
+                </div>
+                <div>
+                    <img src="bookwithroses.jpg" alt="A book with roses and flowers"/>
+                </div>
+                <div>
+                    <img src="girlreading.jpg" alt="A young girl sitting on a large stack of books reading a story"/>
+                </div>
+                <div>
+                    <img src="sparklybook.jpg" alt="A sprakly exciting book"/>
+                </div>
+                <div>
+                    <img src="blurredbooks.jpg" alt="A stack of exciting stories waiting to be read"/>
+                </div>
+                <div>
+                    <img src="blurredlibrary.jpg" alt="A big library filled with books and stories"/>
+                </div>
+                <div>
+                    <img src="lightbookstack.jpg" alt="A pile of books for all ages"/>
+                </div> 
+            </Carousel>
         </Container>
     );
 }
