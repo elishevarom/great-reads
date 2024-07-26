0faf661b547f7b6e56bcf28c52e33dde71710f6a
Menucha Rooshanshad <menucharooshanshad@gmail.com>
2024-07-26 10:55:34 -0400

    resolving conflicts

--- greatreads/src/layout/layout.jsx
+++ greatreads/src/layout/layout.jsx
@@ -20,6 +20,20 @@ export function Layout() {
                 <Nav.Link as={Link} to="/addBook" className="text-light">Add a Book</Nav.Link>
                 <Nav.Link as={Link} to="/addReview" className="text-light">Add Your Review</Nav.Link>
                 <Nav.Link as={Link} to="/findBook" className="text-light">Find a Book</Nav.Link>
+                {/* <Nav.Link as={Link} to="/about" className="text-light">About</Nav.Link>
+                <Nav.Link as={Link} to="/add" className="text-light">Add</Nav.Link>
+                <Nav.Link as={Link} to="/update" className="text-light">Update</Nav.Link>
+                <Nav.Link as={Link} to="/retrieve" className="text-light">Retrieve</Nav.Link>
+                <Nav.Link as={Link} to="/pay" className="text-light">Membership</Nav.Link>
+                <Nav.Link as={Link} to="/delete" className="text-light">Delete</Nav.Link>
+                <Nav.Link as={Link} to="/reviewsDashboard" className="text-light">Reviews Dashboard</Nav.Link> {/* Add this line */}
+                {/* <NavDropdown title="More" id="basic-nav-dropdown" className="text-light">
+                  <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
+                </NavDropdown> */} 
               </Nav>
             </Navbar.Collapse>
           </Container>
