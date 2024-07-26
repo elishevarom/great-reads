5ec714ab6bdc3627b676a59016099ac3156b4993
Menucha Rooshanshad <menucharooshanshad@gmail.com>
2024-07-26 12:23:19 -0400

    resolving

--- greatreads/src/App.js
+++ greatreads/src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path = "update" element = {<Update />} />
           <Route path = "retrieve" element = {<Retrieve />} />
          //<Route path="pay" element={<Pay />} />
-          <Route path="payment" element={<StripePayment />} /> */
+          <Route path="payment" element={<StripePayment />} /> 
           <Route path="success" element={<PaymentSuccess />} /> 
           <Route path = "delete" element = {<Delete />} />
           </Route>
