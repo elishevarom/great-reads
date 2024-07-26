4b410bd2a88d1bf3b00b7298d2db4da1cc54a0bf
Menucha Rooshanshad <menucharooshanshad@gmail.com>
2024-07-26 11:45:56 -0400

    trying to resolve

--- greatreads/src/App.js
+++ greatreads/src/App.js
@@ -13,27 +13,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     //<div className="App">
-         <BrowserRouter>
-         <Routes>
-           <Route path = "/" element={<Layout />}>
-           <Route index element = {<Home />} />
-           <Route path = "about" element = {<About />} />
-           <Route path = "addBook" element = {<Add />} />
-           <Route path = "addReview" element = {<Update />} />
-           <Route path = "findBook" element = {<Retrieve />} />
-           <Route path = "add" element = {<Add />} />
-           <Route path = "update" element = {<Update />} />
-           <Route path = "retrieve" element = {<Retrieve />} /> 
-           <Route path = "retrieve" element = {<Retrieve />} />
-          //<Route path="pay" element={<Pay />} />
-           <Route path="payment" element={<StripePayment />} />
-           <Route path="success" element={<PaymentSuccess />} />
-           <Route path = "delete" element = {<Delete />} />
-           <Route path="reviewsDashboard" element={< ReviewsDashboard />} /> {/* Add this line */}
-           </Route>
-         </Routes>
-         </BrowserRouter>
       
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/" element={<Layout />}>
+            <Route index element = {<Home />} />
+            <Route path = "about" element = {<About />} />
+            <Route path = "addBook" element = {<Add />} />
+            <Route path = "addReview" element = {<Update />} />
+            <Route path = "findBook" element = {<Retrieve />} />
+            <Route path = "add" element = {<Add />} />
+            <Route path = "update" element = {<Update />} />
+            <Route path = "retrieve" element = {<Retrieve />} />
+          //<Route path="pay" element={<Pay />} />
+            <Route path="payment" element={<StripePayment />} />
+            <Route path="success" element={<PaymentSuccess />} />
+            <Route path = "delete" element = {<Delete />} />
+            <Route path="reviewsDashboard" element={< ReviewsDashboard />} /> {/* Add this line */}
+            </Route>
+          </Routes>
+          </BrowserRouter>
+
     //</div>
   );
 }
